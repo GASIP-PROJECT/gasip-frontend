1a01d4e92474dbb3ee352f95872a8813d2284a91
majih93 <t90laser93@gmail.com>
2024-03-19 23:41:25 +0900

    chore: 절대경로 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+          src: './src',
+          // '@assets': './src/assets',
+          // '@components': './src/components',
+          // '@constants': './src/constants',
+          // '@utils': './src/utils',
+          // '@common': './src/common',
+          // '@api': './src/api',
+          '@screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
