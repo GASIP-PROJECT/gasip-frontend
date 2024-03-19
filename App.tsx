4fb76f9db2a5c225fc721016c518752be51ea328
majih93 <t90laser93@gmail.com>
2024-03-19 23:12:39 +0900

    chore: React Navigation 라이브러리 추가 및 설정

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
+        <View
+          style={{
+            backgroundColor: 'slateblue',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Appfss</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
