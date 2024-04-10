b04b8c74b0964ca9013afdfd751ec080f32cf975
majih93 <t90laser93@gmail.com>
2024-04-10 21:25:23 +0900

    feat: 홈페이지 피드 목록 탭 구현

---
+++ src/screens/HomeScreen/FeedTab/FeedSummary.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import Spacer from '@components/common/Spacer';
+
+import { COLORS } from '@styles/colors';
+
+import icon_like from '@assets/icon_like.png';
+import icon_comments from '@assets/icon_comments.png';
+import icon_view from '@assets/icon_view.png';
+
+interface FeedSummaryProps {
+  content: string;
+  likeCount: number;
+  clickCount: number;
+  regDate: string;
+}
+
+export default function FeedSummary({
+  content,
+  likeCount,
+  clickCount,
+  regDate,
+}: FeedSummaryProps) {
+  const handleSummaryPress = () => {
+    // TODO - 각 글 세부 내용으로 이동시키는 처리
+  };
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={handleSummaryPress}>
+      <SummaryHeader regDate={regDate} userNickName="userNickName" />
+      <Spacer type="height" value={10} />
+      <SummaryContent content={content} />
+      <Spacer type="height" value={10} />
+      <SummaryFooter clickCount={clickCount} likeCount={likeCount} />
+      <Spacer type="height" value={10} />
+      <View style={styles.bottomLine} />
+    </TouchableOpacity>
+  );
+}
+
+const SummaryHeader = ({
+  regDate,
+  userNickName,
+}: {
+  regDate: string;
+  userNickName: string;
+}) => {
+  // TODO - 함수 구현
+  const getFeedCreatedTimeString = () => {
+    return regDate;
+  };
+
+  return (
+    <Text style={styles.feedHeaderText}>
+      {getFeedCreatedTimeString()} | {userNickName}
+    </Text>
+  );
+};
+
+const SummaryContent = ({ content }: { content: string }) => {
+  return (
+    <Text style={styles.feedContentText} numberOfLines={2}>
+      {content}
+    </Text>
+  );
+};
+
+const SummaryFooter = ({
+  clickCount,
+  likeCount,
+}: {
+  clickCount: number;
+  likeCount: number;
+}) => {
+  return (
+    <View style={styles.footerContainer}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image source={icon_like} style={{ width: 15, height: 15 }} />
+        <Spacer type="width" value={5} />
+        <Text style={{ fontSize: 15, color: 'red', fontWeight: '500' }}>
+          {likeCount}
+        </Text>
+      </View>
+
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image source={icon_comments} style={{ width: 15, height: 15 }} />
+        <Spacer type="width" value={5} />
+        <Text style={{ fontSize: 15, color: '#4490d8', fontWeight: '500' }}>
+          {3}
+        </Text>
+      </View>
+
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image source={icon_view} style={{ width: 15, height: 15 }} />
+        <Spacer type="width" value={5} />
+        <Text style={{ fontSize: 15, color: '#999999', fontWeight: '500' }}>
+          {clickCount}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingTop: 15,
+    paddingBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: '#28292A',
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bottomLine: {
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.WHITE,
+  },
+  feedHeaderText: {
+    color: '#4b5159',
+    fontWeightt: '500',
+  },
+  feedContentText: {
+    color: COLORS.WHITE,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
--- src/screens/HomeScreen/FeedTab/FeedTab.tsx
+++ src/screens/HomeScreen/FeedTab/FeedTab.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, View } from 'react-native';
+import axios from 'axios';
+
+import FeedSummary from './FeedSummary';
+
+interface Feed {
+  content: string;
+  likeCount: number;
+  clickCount: number;
+  regDate: string;
+}
 
 export default function FeedTab() {
+  const [feedsList, setFeedsList] = useState([]);
+
+  useEffect(() => {
+    const fetchFeeds = async () => {
+      try {
+        const posts = await axios.get('https://gasip.site/boards/0');
+
+        setFeedsList(posts.data.response);
+      } catch (error) {}
+    };
+
+    fetchFeeds();
+  }, []);
+
   return (
-    <View>
-      <Text style={{ color: 'white' }}>FEED</Text>
+    <View style={{ marginBottom: 30 }}>
+      <FlatList
+        data={feedsList}
+        renderItem={({ item }: { item: Feed }) => (
+          <FeedSummary
+            content={item.content}
+            likeCount={item.likeCount}
+            clickCount={item.clickCount}
+            regDate={item.regDate}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+        ListFooterComponent={() => <View style={{ height: 100 }} />}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
