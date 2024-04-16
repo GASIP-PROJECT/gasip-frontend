82addecadb689a1f57f65d6c837298f974d80045
majih93 <t90laser93@gmail.com>
2024-04-16 23:58:01 +0900

    feat: 공통헤더 GSHeader 컴포넌트 추가

---
+++ src/components/common/GSHeader.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { COLORS } from '@styles/colors';
+
+interface GSHeaderProps {
+  title: string;
+  leftComponent?: React.ReactNode | null;
+  onLeftComponentPress?: (() => void) | null;
+  rightComponent?: React.ReactNode | null;
+  onRightComponentPress?: (() => void) | null;
+}
+
+export default function GSHeader({
+  title,
+  leftComponent = null,
+  onLeftComponentPress = null,
+  rightComponent = null,
+  onRightComponentPress = null,
+}: GSHeaderProps) {
+  return (
+    <View style={styles.container}>
+      <HeaderLeftComponent
+        component={leftComponent}
+        onPress={onLeftComponentPress}
+      />
+      <HeaderTitle title={title} />
+      <HeaderRightComponent />
+    </View>
+  );
+}
+
+const HeaderLeftComponent = ({
+  component,
+  onPress,
+}: {
+  component: React.ReactNode;
+  onPress: (() => void) | null;
+}) => {
+  if (component === null) return <View style={styles.leftComponentContainer} />;
+
+  return (
+    <View style={styles.leftComponentContainer}>
+      <TouchableOpacity style={{ flex: 1 }} onPress={onPress || undefined}>
+        {component}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const HeaderTitle = ({ title }: { title: string }) => {
+  return (
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+const HeaderRightComponent = () => {
+  return <View style={styles.rightComponentContainer} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 50,
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  titleContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '800',
+    textAlign: 'center',
+    color: COLORS.WHITE,
+  },
+
+  leftComponentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  rightComponentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
