6f2fe1077f0258c22ceb805a75bd4c2f42fd11eb
majih93 <t90laser93@gmail.com>
2024-04-20 01:14:56 +0900

    feat: 특정 교수 피드 조회 API 추가 및 API코드 정리

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,6 +9,11 @@ export const getAllFeeds = async (page: number, dataCount: number = 5) => {
   try {
     const posts = await GSBackendClient.get(
       `/boards/all-boards?page=${page}&size=${dataCount}`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      },
     );
 
     return posts.data.response;
@@ -22,11 +27,16 @@ export const getGeneralFeeds = async (page: number, dataCount: number = 5) => {
   try {
     const posts = await GSBackendClient.get(
       `/boards/0?page=${page}&size=${dataCount}`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      },
     );
 
     return posts.data.response;
   } catch (error) {
-    console.log('getAllFeeds error: ', error);
+    console.log('getGeneralFeeds error: ', error);
     return [];
   }
 };
@@ -35,6 +45,11 @@ export const getPopularFeeds = async (page: number, dataCount: number = 5) => {
   try {
     const posts = await GSBackendClient.get(
       `/boards/best?page=${page}&size=${dataCount}`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      },
     );
 
     return posts.data.response;
@@ -44,9 +59,31 @@ export const getPopularFeeds = async (page: number, dataCount: number = 5) => {
   }
 };
 
+export const getProfessorFeeds = async (
+  profId: number,
+  page: number,
+  dataCount: number = 5,
+) => {
+  try {
+    const posts = await GSBackendClient.get(
+      `/boards/${profId}?page=${page}&size=${dataCount}`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      },
+    );
+
+    return posts.data.response;
+  } catch (error) {
+    console.log('getProfessorFeeds error: ', error);
+    return [];
+  }
+};
+
 // TODO - 테스트용 토큰
 const access_token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3IiwiZW1haWwiOiJqaUB0ZXN0LmNvbSIsInJvbGVzIjpbIk1FTUJFUiJdLCJleHAiOjE3MTMzNjAzNzB9.7YoW_Epi1UDXcTpydv1Odj84C6oWuhA2fjDJjcTpeIk';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3IiwiZW1haWwiOiJqaUB0ZXN0LmNvbSIsInJvbGVzIjpbIk1FTUJFUiJdLCJleHAiOjE3MTQwNTE3MzV9.yrMNkYMcJH0zuctnl5dQLgBMdk7GTHQq23kB6X-q0S4';
 
 export const createFeed = async (content: string) => {
   try {
@@ -173,5 +210,42 @@ export const getUserData = async () => {
   } catch (error) {
     console.log('getUserData error: ', error);
     return null;
+  }
+};
+
+export const likeFeed = async (postId: number) => {
+  try {
+    const response = await GSBackendClient.post(
+      `/boards/likes`,
+      {
+        postId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      },
+    );
+
+    console.log(response.data);
+  } catch (error) {
+    console.log('likeFeed error: ', error);
+  }
+};
+
+export const likeFeedCancel = async (postId: number) => {
+  try {
+    const response = await GSBackendClient.delete(`/boards/likes`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+      data: {
+        postId,
+      },
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.log('likeFeedCancel error: ', error);
   }
 };
