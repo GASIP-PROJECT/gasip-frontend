3f194da5c2322a56fe13198d8d25e60c7e3ba246
majih93 <t90laser93@gmail.com>
2024-04-21 16:10:51 +0900

    fix: GSButton 스타일 수정

--- src/components/common/GSButton.tsx
+++ src/components/common/GSButton.tsx
@@ -6,18 +6,30 @@ import { COLORS } from '@styles/colors';
 interface GSButtonProps {
   buttonText: string;
   onPress: () => void | Promise<void> | null;
+  bgColor?: string;
+  btnTextColor?: string;
+  marginHorizontal?: number;
 }
 
 // 태헌님 이 공통으로 사용할 버튼 컴포넌트입니다.
 // 컴포넌트에 표시될 텍스트와, 눌렀을 때 실행될 함수를 전달해서 구현하면 됩니다.
 // ex) <GSButton buttonText="Gasip 로그인" onPress={handleLogin} />
-export default function GSButton({ buttonText, onPress }: GSButtonProps) {
+export default function GSButton({
+  buttonText,
+  onPress,
+  bgColor = COLORS.BTN_MAIN,
+  btnTextColor = COLORS.WHITE,
+  marginHorizontal = 16,
+}: GSButtonProps) {
   return (
     <TouchableOpacity
-      style={[styles.container, { marginHorizontal: 16 }]}
+      style={[
+        styles.container,
+        { marginHorizontal: marginHorizontal, backgroundColor: bgColor },
+      ]}
       onPress={onPress}
     >
-      <Text style={styles.text}>{buttonText}</Text>
+      <Text style={[styles.text, { color: btnTextColor }]}>{buttonText}</Text>
     </TouchableOpacity>
   );
 }
@@ -28,10 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 5,
-    backgroundColor: COLORS.BTN_MAIN,
   },
   text: {
-    color: COLORS.WHITE,
     fontSize: 18,
     fontWeight: '700',
   },
