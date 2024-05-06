f89f3b41635d576d8869e8c6d1ce8cec3ebf1217
majih93 <t90laser93@gmail.com>
2024-05-06 23:04:38 +0900

    feat: 교수 상세 페이지에서 피드 작성 모달 오픈 시 profId 전달되도록 처리

--- src/contexts/NewFeedContext.tsx
+++ src/contexts/NewFeedContext.tsx
@@ -10,9 +10,10 @@ import {
 interface NewFeedContextValueType {
   showCreateFeedModal: boolean;
   toggleToUpdateFeedsList: boolean;
+  profId: number | null;
   // setShowCreateFeedModal: Dispatch<SetStateAction<boolean>>;
   // setToggleToUpdateFeedsList: Dispatch<SetStateAction<boolean>>;
-  openNewFeedModal: () => void;
+  openNewFeedModal: (profId?: number | null) => void;
   closeNewFeedModal: () => void;
   triggerFeedListUpdate: () => void;
 }
@@ -20,6 +21,7 @@ interface NewFeedContextValueType {
 const NewFeedContext = createContext<NewFeedContextValueType>({
   showCreateFeedModal: false,
   toggleToUpdateFeedsList: false,
+  profId: null,
   // setToggleToUpdateFeedsList: () => {},
   openNewFeedModal: () => {},
   closeNewFeedModal: () => {},
@@ -30,12 +32,20 @@ export const NewFeedProvider = ({ children }: { children: ReactNode }) => {
   const [toggleToUpdateFeedsList, setToggleToUpdateFeedsList] = useState(false);
   const [showCreateFeedModal, setShowCreateFeedModal] = useState(false);
 
-  const openNewFeedModal = () => {
+  // 특정 교수에 대한 피드 작성 위해서 교수 id담는 state
+  const [profId, setProfId] = useState<number | null>(null);
+
+  const openNewFeedModal = (profId: number | null = null) => {
+    if (profId !== null) {
+      setProfId(profId);
+    }
+
     setShowCreateFeedModal(true);
   };
 
   const closeNewFeedModal = () => {
     setShowCreateFeedModal(false);
+    setProfId(null);
   };
 
   // 새로운 피드 작성 후 피드 목록을 업데이트하기 위해 상태 변경
@@ -48,6 +58,7 @@ export const NewFeedProvider = ({ children }: { children: ReactNode }) => {
       value={{
         showCreateFeedModal,
         toggleToUpdateFeedsList,
+        profId,
         openNewFeedModal,
         closeNewFeedModal,
         triggerFeedListUpdate,
