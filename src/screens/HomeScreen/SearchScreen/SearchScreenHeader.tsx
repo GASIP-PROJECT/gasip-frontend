e1863ffca159387d98602f5f5c7ba2d08b4bc6e2
majih93 <t90laser93@gmail.com>
2024-05-15 11:42:03 +0900

    feat: 앱 내 로고 Text로고로 변경

--- src/screens/HomeScreen/SearchScreen/SearchScreenHeader.tsx
+++ src/screens/HomeScreen/SearchScreen/SearchScreenHeader.tsx
@@ -3,7 +3,7 @@ import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import icon_user from '@assets/icon_user.png';
-import gasip_logo from '@assets/gasip_logo.png';
+import gasip_text from '@assets/gasip_text.png';
 import icon_goback from '@assets/icon_goback.png';
 
 export default function SearchScreenHeader() {
@@ -19,7 +19,7 @@ export default function SearchScreenHeader() {
       </TouchableOpacity>
 
       <Image
-        source={gasip_logo}
+        source={gasip_text}
         style={{ width: 100, height: 40 }}
         resizeMode="contain"
       />
