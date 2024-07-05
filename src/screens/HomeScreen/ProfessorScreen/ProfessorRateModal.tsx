d3c60e5af6905845aeb6da2793824fdddfefd924
majih93 <t90laser93@gmail.com>
2024-07-06 01:21:58 +0900

    [UI수정]평점 입력 모달 숫자 고정간격으로 수정

--- src/screens/HomeScreen/ProfessorScreen/ProfessorRateModal.tsx
+++ src/screens/HomeScreen/ProfessorScreen/ProfessorRateModal.tsx
@@ -170,6 +170,7 @@ const styles = StyleSheet.create({
   currentRatingText: {
     fontSize: 24,
     fontWeight: '500',
+    fontVariant: ['tabular-nums'],
   },
   ratingContainer: {
     flexDirection: 'row',
