84f6d835a055d71979d290d0e9f266d536334322
majih93 <t90laser93@gmail.com>
2024-08-08 21:38:29 +0900

    [UI수정] 아이폰8 화면에서 하단 버튼 간격 조정(재수정 필요)

--- src/hooks/useGetBottomButtonGap.ts
+++ src/hooks/useGetBottomButtonGap.ts
@@ -1,10 +1,14 @@
 import { Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+// TODO - 심사 위한 스크린샷 찍기 위해서 iOS코드 임시로 추가함 -> 제대로 알아보고 수정 필요하다.
 export default function useGetBottomButtonGap() {
   const { bottom } = useSafeAreaInsets();
+  const insets = useSafeAreaInsets();
 
-  const bottomButtonGap = Platform.OS === 'android' ? 15 : bottom;
+  const iOSBottomGap = insets.bottom === 0 ? 16 : bottom;
+
+  const bottomButtonGap = Platform.OS === 'android' ? 15 : iOSBottomGap;
 
   return { bottomButtonGap };
 }
