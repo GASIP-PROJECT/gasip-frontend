4e83dc1c5e9ac25656595655e7790db6fc035a46
majih93 <t90laser93@gmail.com>
2024-08-22 01:11:38 +0900

    feat(피드신고): 피드 신고 모달 UI 구현

---
+++ src/screens/HomeScreen/FeedDetailScreen/FeedReportModal.tsx
@@ -0,0 +1,174 @@
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { StyleSheet, Modal, View, TextInput } from 'react-native';
+
+import FeedActionModalHeader from './FeedActionModalHeader';
+
+import GSText from '@components/common/GSText';
+import Spacer from '@components/common/Spacer';
+import SafeAreaLayout from '@components/common/SafeAreaLayout';
+
+import { COLORS } from '@styles/colors';
+
+interface FeedEditModalProps {
+  isVisible: boolean;
+  reportData: {
+    content: string;
+    authorNickname: string;
+    type: '게시글' | '댓글';
+  };
+  closeModal: () => void;
+}
+
+export default function FeedReportModal({
+  reportData,
+  isVisible,
+  closeModal,
+}: FeedEditModalProps) {
+  const [reasonForReporting, setReasonForReporting] = useState('');
+
+  // TODO - 신고 시 백엔드 구현 후 API 붙여서 기능 구현 필요
+  const handleReportCompletePress = () => {};
+
+  const handleCloseModalPress = () => {
+    closeModal();
+    setReasonForReporting('');
+  };
+
+  return (
+    <Modal visible={isVisible} animationType="slide">
+      <SafeAreaLayout>
+        <FeedActionModalHeader
+          title="신고하기"
+          handleActionButtonPress={() => {
+            console.log('신고 완료!!');
+            handleCloseModalPress();
+          }}
+          closeModal={handleCloseModalPress}
+        />
+
+        <View style={styles.container}>
+          <Spacer type="height" value={16} />
+
+          <GSText style={styles.subTitle}>신고 {reportData.type}</GSText>
+          <Spacer type="height" value={8} />
+          <ReportedContent
+            content={reportData.content}
+            authorNickname={reportData.authorNickname}
+          />
+
+          <Spacer type="height" value={12} />
+          <View style={styles.divider} />
+          <Spacer type="height" value={12} />
+
+          <GSText style={styles.subTitle}>신고 이유</GSText>
+          <Spacer type="height" value={8} />
+          <ReportReasonInput
+            reasonForReporting={reasonForReporting}
+            setReasonForReporting={setReasonForReporting}
+          />
+          <View style={styles.divider} />
+
+          <Spacer type="height" value={32} />
+          <ReportInformation />
+        </View>
+      </SafeAreaLayout>
+    </Modal>
+  );
+}
+
+const ReportedContent = ({
+  content,
+  authorNickname,
+}: {
+  content: string;
+  authorNickname: string;
+}) => {
+  return (
+    <View style={styles.contentContainer}>
+      <GSText style={styles.authorNickname}>{authorNickname}</GSText>
+      <Spacer type="height" value={12} />
+      <GSText style={styles.content}>{content}</GSText>
+    </View>
+  );
+};
+
+const ReportReasonInput = ({
+  reasonForReporting,
+  setReasonForReporting,
+}: {
+  reasonForReporting: string;
+  setReasonForReporting: Dispatch<SetStateAction<string>>;
+}) => {
+  return (
+    <TextInput
+      placeholder="신고 사유를 작성해주세요."
+      style={{ height: 100, width: '100%', fontSize: 14, color: '#111111' }}
+      onChangeText={text => setReasonForReporting(text)}
+      value={reasonForReporting}
+      multiline
+    />
+  );
+};
+
+const ReportInformation = () => {
+  return (
+    <View style={styles.reportInformationContainer}>
+      <GSText style={styles.reportInformationText}>
+        신고 건은 검토 후에 24시간 이내에 신속히 처리하겠습니다.
+      </GSText>
+      <Spacer type="height" value={3} />
+      <GSText style={styles.reportInformationText}>
+        5회 이상 신고당한 유저는 제재 처리 됩니다.
+      </GSText>
+      <Spacer type="height" value={3} />
+      <GSText style={styles.reportInformationText}>
+        청렴한 커뮤니티 문화를 위한 여러분의 소중한 의견 감사합니다.
+      </GSText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  subTitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: COLORS.GRAY_500,
+    alignSelf: 'flex-start',
+  },
+  contentContainer: {
+    width: '100%',
+    padding: 16,
+    backgroundColor: COLORS.GRAY_100,
+    borderRadius: 8,
+  },
+  authorNickname: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: '#111111',
+  },
+  content: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#111111',
+  },
+  divider: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#E5E9EE',
+  },
+  reportInformationContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  reportInformationText: {
+    fontSize: 12,
+    fontWeight: '400',
+    color: COLORS.GRAY_400,
+  },
+});
